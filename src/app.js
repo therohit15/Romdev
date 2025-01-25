e9cd239db29841a78c1f027d52ed82e095c43062
Rohit <132490663+therohit15@users.noreply.github.com>
2025-01-25 06:17:23 +0000

    route_and_reqhandles_ls

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const port = 3000;
-app.use('/test',(req,res)=>{
-    res.send("Test successful");
-})
-app.use("/hello",(req,res)=>{
-    res.send("hello hello hello");
-})
-app.use("/",(req,res)=>{
-    res.send("Hello world");
+app.get("/user",(req,res)=>{
+    res.send({
+        "firstname":"Rohit",
+        "lastname": "K"
+    })
+});
+app.post("/user",(req,res)=>{
+    res.send("User successfully saved to the database");
+});
+app.delete("/user",(req,res)=>{
+    res.send("User successfully deleted from the database");
 })
 app.listen(port,()=>{
     console.log("Server is successfully listening on port 3000");
