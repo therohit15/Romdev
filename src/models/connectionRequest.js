6ea6bbce08d6f91a0f7542c3e83730bd8c1f3d8c
Rohit <132490663+therohit15@users.noreply.github.com>
2025-02-23 10:14:30 +0000

    dbQueryLs

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -18,6 +18,13 @@ const connectionRequestSchema = new mongoose.Schema({
 },{
     timestamps:true,
 });
+connectionRequestSchema.pre("save", function(next){
+    const connectionRequest = this;
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send connection request to yourself");
+    }
+    next();
+})
 const ConnectionRequest = new mongoose.model("ConnectionRequest",connectionRequestSchema);
 module.exports={
     ConnectionRequest,
--- src/routes/request.js
+++ src/routes/request.js
@@ -2,6 +2,7 @@ const express = require("express");
 const requestRouter = express.Router();
 const {userAuth} = require("../middlewares/auth.js");
 const {ConnectionRequest} = require("../models/connectionRequest.js");
+const User = require("../models/user.js");
 requestRouter.post("/request/send/:status/:userId", userAuth, async (req,res)=>{
     try {
         const fromUserId = req.user._id;
@@ -20,6 +21,12 @@ requestRouter.post("/request/send/:status/:userId", userAuth, async (req,res)=>{
         if (existingConnectionRequest) {
             return res.status(400).json({ message: "Connection Request Already Exists" });
         }
+        const toUser = await User.findById(toUserId);
+        if(!toUser){
+            return res.status(400).json({
+                message:"toUser is not found",
+            });
+        }
         const connectionRequest = new ConnectionRequest({
             fromUserId,
             toUserId,
@@ -27,8 +34,8 @@ requestRouter.post("/request/send/:status/:userId", userAuth, async (req,res)=>{
         });
         const data = await connectionRequest.save();
         return res.status(201).json({
-            message: "Connection request sent successfully",
-            request: savedRequest,
+            message: req.user.firstName +" is "+status+" in "+toUser.firstName,
+            data,
         });
     } catch (err) {
         res.status(400).send("Error : "+ err.message);
