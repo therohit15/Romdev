84ec904ad8e3cd4a0e8f2d9f23343ce5f7525485
Rohit <132490663+therohit15@users.noreply.github.com>
2025-02-27 18:39:00 +0000

    Added Navbar Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-function App() {
+import Navbar from "./components/Navbar";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Body from "./pages/Body";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
 
+function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
