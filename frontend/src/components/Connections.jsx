640e5ba965b4411ee62c7c202c076e9e79a67dfd
Rohit <132490663+therohit15@users.noreply.github.com>
2025-07-07 07:03:55 +0000

    added connections page

---
+++ frontend/src/components/Connections.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { addConnections } from "../utils/connectionSlice";
+
+const Connections = () => {
+  const connections = useSelector((store) => store.connections);
+  const dispatch = useDispatch();
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/connections", {
+        withCredentials: true,
+      });
+      dispatch(addConnections(res.data.data));
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
+  useEffect(() => {
+    fetchConnections();
+  }, []);
+  if (!connections) return;
+  if (connections.length === 0) {
+    return (
+      <div className="text-center my-10">
+        <h1 className="text-bold text-2xl">No Connections Found</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="text-center my-10">
+      <h1 className="text-bold text-3xl">Connections</h1>
+      {connections.map((connection) => {
+        const { firstName, lastName, photoUrl, gender, age, bio } = connection;
+        return (
+          <div className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
+            <div>
+              <img
+                src={photoUrl}
+                alt="photo"
+                className="rounded-full w-20 h-20"
+              />
+            </div>
+            <div className="text-left mx-4">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+              {age && gender && <p>{age + ", " + gender}</p>}
+              <p>{bio}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Connections;
